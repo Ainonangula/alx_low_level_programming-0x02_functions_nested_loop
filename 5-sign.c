aca6fb45ecc9a958515a4b142eb5cb4b4d16369e
Ainonangula <114227170+Ainonangula@users.noreply.github.com>
2022-10-19 12:14:46 +0200

    Update 5-sign.c

--- 5-sign.c
+++ 5-sign.c
@@ -4,7 +4,7 @@
   * @n: the number of the sign to be printed 
   * Return:1 if the number is greater than zero 
   *        0 if the number is zero 
-  *        -1 if number is less that zero 
+  *       -1 if number is less that zero 
   /* 
   int print_sign(int n) 
   {
@@ -13,7 +13,7 @@
        _putchar ('+');
       Return (1) 
    } 
-     else if ( n == 0)
+     else if (n == 0)
   {
      _putchar ('0') ;
      Return (0)
